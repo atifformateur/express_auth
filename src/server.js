7542653b502d40ba4c2dc891aa21190146557494
atifformateur <atif.zourgani@laplateforme.io>
2025-11-26 09:33:53 +0100

    server.js

--- src/server.js
+++ src/server.js
@@ -1,31 +1,13 @@
-import app from "./app.js";
-import {env} from "./config/env.js";
-import { testConnection, pool } from "./db/index.js";
+import app from './app.js';
+import { env } from './config/env.js';
+import { testConnection } from './db/index.js';
 
 async function start() {
-    try {
-        await testConnection(); //notre fonction qui verifie si la connexion est ok 
-        app.listen(env.port, ()=>{
-            console.log(`serveur pret sur http://localhost:${env.port}`);
-        })
-    } catch (error) {
-        console.error('lancement server impossible', error);
-        process.exit(1);
-    }
-}
+    await testConnection();
 
-import {fileURLToPath} from 'url';
-const thisFile = fileURLToPath(import.meta.url);
-
-if(thisFile === process.argv[1]) {
-    testConnection().then(()=>{
-        console.log('test terminé');
-        return pool.end();
-    })
-    .catch((error)=>{
-        console.error('test manuel echoué', error);
-        process.exit(1);
+    app.listen(env.port, ()=>{
+        console.log(`serveur lancé sur le port ${env.port}`);
     })
 }
 
-// start();
+start();
